0b6e83f01c52e9343a4c2f44acca5bcbc989b44d
AnishXD <110338891+AnishXD@users.noreply.github.com>
2024-09-11 22:58:37 +0530

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { getOtherUsers, login, logout, register } from "../controllers/userController.js";
-import isAuthenticated from "../middleware/isAuthenticated.js";
+// import isAuthenticated from "../middleware/isAuthenticated.js"; // No longer needed
 
 const router = express.Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
-router.route("/").get(isAuthenticated,getOtherUsers);
+router.route("/").get(getOtherUsers); // Removed isAuthenticated middleware
 
 export default router;
